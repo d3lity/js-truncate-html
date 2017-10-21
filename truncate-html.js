7c0a06d8f5d15c9f1fff1d7fd4e62bd5ee8332be
d3lity <d3lity@live.com>
2017-10-21 11:39:26 +0300

    singleton tags
    There is tags that don't need to be closed..

--- truncate-html.js
+++ truncate-html.js
@@ -4,7 +4,7 @@ function cut_html(txt,n){
   // remove opened+closed tags
   var tx=t.replace(/<(.*?[^\/])>.*?<\/\1>|<.*?\/>/,"");
   // capture open tags
-  var ar=tx.match(/<(.*?)>/g);
+  var ar=tx.match(/<((?!li|hr|img|br|area|base|col|command|embed|input|keygen|link|meta|head|param|source|track|wbr).*?)>/g);
   // close tags
   if (ar) t=t+"&hellip;"+ar.join("").replace(/>/,"\/>");
 	else t=txt;
